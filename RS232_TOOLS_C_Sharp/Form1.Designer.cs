179786f7a606098c8aa179ac69b2c4c304a6f435
ryanming9635 <ryan.ming999@gmail.com>
2022-05-31 17:40:47 +0800

    例行性備份：20220531

--- RS232_TOOLS_C_Sharp/Form1.Designer.cs
+++ RS232_TOOLS_C_Sharp/Form1.Designer.cs
@@ -29,11 +29,217 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtReceiveData = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.TimeClock = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(283, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "連接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(283, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 36);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "中斷";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(145, 32);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "115200"});
+            this.comboBox2.Location = new System.Drawing.Point(145, 80);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 28);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(45, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "COM PORT";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(42, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "BAUD RATE";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button3.Location = new System.Drawing.Point(793, 25);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 35);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "離開";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtReceiveData);
+            this.groupBox1.Location = new System.Drawing.Point(12, 128);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(862, 530);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button4.Location = new System.Drawing.Point(12, 111);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(115, 39);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Send CMD";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(143, 121);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 29);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.Text = "FW";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(445, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 12);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "ReceiveData";
+            // 
+            // txtReceiveData
+            // 
+            this.txtReceiveData.BackColor = System.Drawing.SystemColors.InfoText;
+            this.txtReceiveData.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtReceiveData.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtReceiveData.Location = new System.Drawing.Point(447, 33);
+            this.txtReceiveData.Name = "txtReceiveData";
+            this.txtReceiveData.Size = new System.Drawing.Size(409, 329);
+            this.txtReceiveData.TabIndex = 11;
+            this.txtReceiveData.Text = "";
+            this.txtReceiveData.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtReceiveData_KeyPress);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // TimeClock
+            // 
+            this.TimeClock.AutoSize = true;
+            this.TimeClock.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.TimeClock.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.TimeClock.Location = new System.Drawing.Point(469, 32);
+            this.TimeClock.Name = "TimeClock";
+            this.TimeClock.Size = new System.Drawing.Size(75, 26);
+            this.TimeClock.TabIndex = 12;
+            this.TimeClock.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(886, 721);
+            this.Controls.Add(this.TimeClock);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "RS232 Tools";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox txtReceiveData;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label TimeClock;
     }
 }
 
--- RS232_TOOLS_C_Sharp/Form1.cs
+++ RS232_TOOLS_C_Sharp/Form1.cs
@@ -7,14 +7,310 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
 
 namespace RS232_TOOLS_C_Sharp
 {
     public partial class Form1 : Form
     {
-        public Form1()
+
+     //   SerialPort serialPort1 = new SerialPort();
+        delegate void SetTextCallback(string text);
+
+          public Form1()
         {
             InitializeComponent();
         }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            String test = "";
+            if (serialPort1.BytesToRead > 0)
+            {
+                this.ReceiveAuto(test);
+            }
+        }
+
+        private void ReceiveAuto(string test)
+        {
+            if (this.txtReceiveData.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(ReceiveAuto);
+                this.Invoke(d, new object[] { "" });
+            }
+            else
+            {
+                ReceiveData();
+            }
+        }
+
+        private void ReceiveData()
+        {
+            int GetValue;
+            int k = serialPort1.BytesToRead;// -2;
+            //if(k>0)
+            byte[] ReadData = new byte[k];
+            int i = 0;
+            //if (AutoClean.Checked == true) //?芸?皜?交?孵?
+            //{
+            //    //txtReceiveHEX.
+            //    if (CountLength >= CountLengthValue)
+            //    //if (txtReceiveHEX.Text.Length >= CountLengthValue)
+            //    {
+            //        CountLength = 0;
+            //        txtReceiveHEX.Text = "";
+            //        txtReceiveASCII.Text = "";
+            //    }
+            //}
+            //else
+            //{
+            //    if (AutoNewLine.Checked == true && txtReceiveHEX.Text == "")
+            //    {
+            //        txtReceiveHEX.Text += "\n";
+            //        txtReceiveASCII.Text += "\n";
+            //    }
+            //}
+
+            //PRINT("RS232有資料輸入\r\n");
+        
+            if (serialPort1.BytesToRead > 0)
+            {
+                try
+                {
+                    serialPort1.ReadTimeout = 1000;
+                    GetValue = serialPort1.Read(ReadData, 0, k);
+
+                    for (i = 0; i <= k - 1; i++)
+                    {
+                        {
+                            //if (AutoClean.Checked == true) //?芸?皜?交?孵?
+                            //{
+                            //    CountLength++;
+                            //    CountLengthValueDisplay.Text = Convert.ToString(CountLength);
+                            //}
+                            //txtReceiveHEX.Text += String.Format("{0:X2}", ReadData[i]) + ",";
+                            //txtReceiveASCII.Text += (char)ReadData[i];// +",";
+                            if ( (char ) ReadData[i] == '\n' )//取消換行
+                            {
+                            //txtReceiveData.Text += (char) ReadData[i];
+                            }
+                            else
+                            {
+                                txtReceiveData.Text += (char) ReadData[i];
+                            }
+                        }
+
+                    }
+               
+                }
+                catch (Exception )//ex //As Exception
+                {
+                    //MessageBox.Show("霈?隤歹?" + e.ToString(),
+                    //                "RS485?航炊?",
+                    //                MessageBoxButtons.OK,
+                    //                MessageBoxIcon.Exclamation,
+                    //                MessageBoxDefaultButton.Button1);
+                }
+                //txtReceiveHEX.SelectionStart = txtReceiveHEX.Text.Length;
+                //txtReceiveASCII.SelectionStart = txtReceiveASCII.Text.Length;
+                //txtReceiveHEX.ScrollToCaret();
+                //txtReceiveASCII.ScrollToCaret();
+                txtReceiveData.SelectionStart = txtReceiveData.Text.Length;
+                txtReceiveData.ScrollToCaret();
+
+
+            }
+        }
+        private void txtReceiveDataReset()
+            {
+            txtReceiveData.SelectionStart = txtReceiveData.Text.Length;
+            txtReceiveData.ScrollToCaret();
+            }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            string[] ports = SerialPort.GetPortNames();
+
+            DateTime localDate = DateTime.Now;
+            TimeClock.Text = localDate.ToString();
+
+            foreach (string port in ports)
+            {
+               comboBox1.Items.Add(port); 
+            }
+
+            comboBox1.SelectedIndex = 0;
+            comboBox2.SelectedIndex = 0;
+            //StartUp();
+        }
+
+       
+        private void StartUp()
+         {
+         string path = System.Windows.Forms.Application.StartupPath + "/rs232.ini";
+         StreamReader sr = new StreamReader(path , Encoding.Default);
+         String line;
+         while ((line = sr.ReadLine()) != null)
+             {
+             //Console.WriteLine(line.ToString());
+             PRINT (line.ToString());
+             }
+      
+    
+         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+        DateTime localDate = DateTime.Now;
+     
+       string path = System.Windows.Forms.Application.StartupPath + "/rs232.ini";
+        //FileStream fs = new FileStream(path  , FileMode.Create);
+        ////獲得位元組陣列
+        //byte[] data = System.Text.Encoding.Default.GetBytes("Hello World!");
+        ////開始寫入
+        //fs.Write(data , 0 , data.Length);
+
+        //if (serialPort1.IsOpen == true)
+        //    {
+        //    fs.Write(serialPort1.na );
+        //    }
+
+        ////清空緩衝區、關閉流
+        //fs.Flush();
+        //fs.Close();
+
+        FileStream fs = new FileStream(path , FileMode.Create);
+        StreamWriter sw = new StreamWriter(fs);
+        //開始寫入
+        sw.Write(localDate.ToString() + "\r");
+     
+        if (serialPort1.IsOpen == true)
+            {
+            sw.Write(serialPort1.PortName + "\r");
+            }
+
+        //清空緩衝區
+        sw.Flush();
+        //關閉流
+        sw.Close();
+        fs.Close();
+
+        serialPort1.Dispose();
+        this.Close();
+        }
+
+        void PRINT(string str)
+        {
+
+            try
+            {
+                txtReceiveData.AppendText(str + "\n");
+            }
+            catch (Exception)
+            {
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //波特率
+                serialPort1.BaudRate = 115200;
+                //資料位
+                serialPort1.DataBits = 8;
+                serialPort1.PortName = comboBox1.Text;              
+                //兩個停止位
+                serialPort1.StopBits = System.IO.Ports.StopBits.One;
+                //無奇偶校驗位
+                serialPort1.Parity = System.IO.Ports.Parity.None;
+                serialPort1.ReadTimeout = 100;
+                serialPort1.Open();
+
+                if (!serialPort1.IsOpen)
+                {
+                    MessageBox.Show("埠開啟失敗");
+                    return;
+                }
+                else
+                {
+                  //  txtReceiveData.AppendText("埠" + comboBox1.Text + "開啟成功\r\n");
+                }
+                
+                //serialPort1.DataReceived += serialPort1_DataReceived;
+
+                groupBox1.Enabled = true;
+                button1.Enabled = false;
+                comboBox1.Enabled = false;
+                button2.Enabled = true;
+            }
+            catch (Exception )
+            {
+                serialPort1.Dispose();
+                //txtReceiveData.AppendText(ex.Message);
+                MessageBox.Show(serialPort1.PortName + "埠開啟失敗");
+            }
+        }
+
+  
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            groupBox1.Enabled = false;
+
+            try
+            {
+                serialPort1.Dispose();
+
+            }
+            catch (Exception)
+            {
+                ////MessageBox.Show("COM PORT 沒有開啟");
+                //txtReceiveData.AppendText("COM PORT 沒有開啟\r\n");
+            }
+
+            txtReceiveData.Text = "";
+            button1.Enabled = true;
+            button2.Enabled = false;
+            comboBox1.Enabled = true;
+            //comboBox2.Enabled = false;
+       }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write(textBox1.Text + " \r" );
+            txtReceiveData.Text = "";   
+        }
+
+        private void timer1_Tick(object sender , EventArgs e)
+            {
+            DateTime localDate = DateTime.Now;
+            TimeClock.Text = localDate.ToString();// DateTime.Now.ToString("HH:mm:ss");
+            }
+
+        private void txtReceiveData_KeyPress(object sender , KeyPressEventArgs e)
+            {
+            if (Convert.ToInt32(e.KeyChar) == 13)
+                {
+                //MessageBox.Show(" Enter pressed ");
+                serialPort1.Write(textBox1.Text + " \r");
+                textBox1.Text = "";
+
+                txtReceiveDataReset();
+                }
+            else
+                {
+                textBox1.Text += e.KeyChar;         
+                }
+            }
+
+    
+       
+
+              
     }
 }
